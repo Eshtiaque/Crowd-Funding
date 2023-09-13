402bc635256826a29dece6b3054797f0abf151a6
Eshtiaque Ahmed <est.ahmed111@gmail.com>
2023-09-13 16:46:10 +0600

    L

--- src/SocialSite/SocialSite.jsx
+++ src/SocialSite/SocialSite.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import TopBar from "./TopBar/TopBar";
+import SocialFooter from "./SocialFooter/SocialFooter";
 
 const SocialSite = () => {
     return (
         <div>
             <TopBar></TopBar>
             <Outlet/>
+            <SocialFooter></SocialFooter>
         </div>
     );
 };
